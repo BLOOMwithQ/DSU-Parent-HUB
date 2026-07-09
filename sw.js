c069965008fd97b5ec57d53b7c636d01c8006652
BLOOMwithQ <32018225+BLOOMwithQ@users.noreply.github.com>
2026-07-09 18:41:54 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'stem-achievers-parent-hub-v2';
+const URLS_TO_CACHE = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
